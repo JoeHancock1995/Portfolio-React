cc009f513aba8e74f47553ce79f52373f52419a9
Joe Hancock <joe.hancock95@gmail.com>
2020-03-04 21:24:39 -0600

    Updates

---
+++ precache-manifest.72bdcf436b875608d6560bb5bd587908.js
@@ -0,0 +1,370 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4f18d785862a6754d37ee5cbc6818e",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "89e58d854cbbd8f95da7",
+    "url": "/Portfolio-React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5f2ad4cf69cd3330e8af",
+    "url": "/Portfolio-React/static/css/main.b4f7a289.chunk.css"
+  },
+  {
+    "revision": "89e58d854cbbd8f95da7",
+    "url": "/Portfolio-React/static/js/2.a6d1f1f8.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/Portfolio-React/static/js/2.a6d1f1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2ad4cf69cd3330e8af",
+    "url": "/Portfolio-React/static/js/main.e0a133f0.chunk.js"
+  },
+  {
+    "revision": "ae393d7ba428e020319c",
+    "url": "/Portfolio-React/static/js/runtime-main.38861f94.js"
+  },
+  {
+    "revision": "d38d529d36d1ce12ef04c9dc1e2e9491",
+    "url": "/Portfolio-React/static/media/CRASH___.d38d529d.woff2"
+  },
+  {
+    "revision": "fd62ea0143e9bae464387ffb452d9c46",
+    "url": "/Portfolio-React/static/media/CRASH___.fd62ea01.woff"
+  },
+  {
+    "revision": "7fe771a37fc6e170ff3e8065c09e2ba3",
+    "url": "/Portfolio-React/static/media/GentlemanCaller.7fe771a3.woff2"
+  },
+  {
+    "revision": "abe0ede888b11a4945d10d5ed7c98290",
+    "url": "/Portfolio-React/static/media/GentlemanCaller.abe0ede8.woff"
+  },
+  {
+    "revision": "736c85789ab3088ab325d7e96e294488",
+    "url": "/Portfolio-React/static/media/MW_PITHY.736c8578.woff2"
+  },
+  {
+    "revision": "b745f295e8379aca345a62d046a062e2",
+    "url": "/Portfolio-React/static/media/MW_PITHY.b745f295.woff"
+  },
+  {
+    "revision": "5c0eb289092919d3f2dcad3412988077",
+    "url": "/Portfolio-React/static/media/NewMedia.5c0eb289.woff"
+  },
+  {
+    "revision": "c94cac78f7fa9880cbb4e607bd6554ab",
+    "url": "/Portfolio-React/static/media/NewMedia.c94cac78.woff2"
+  },
+  {
+    "revision": "7f52f43736a1d8e7ca49260b90de6768",
+    "url": "/Portfolio-React/static/media/Quad Black.7f52f437.woff2"
+  },
+  {
+    "revision": "8d20640619f64d7b99f17451ecb8c84e",
+    "url": "/Portfolio-React/static/media/Quad Black.8d206406.woff"
+  },
+  {
+    "revision": "49187d1d43dc6d1cc29c0d204324a6d4",
+    "url": "/Portfolio-React/static/media/TANTOR__.49187d1d.woff"
+  },
+  {
+    "revision": "fb93b63df81be52586e37b8dcbde0ab0",
+    "url": "/Portfolio-React/static/media/TANTOR__.fb93b63d.woff2"
+  },
+  {
+    "revision": "1b4c75aeaf5283ddfad09dfacb017ff6",
+    "url": "/Portfolio-React/static/media/TGL 31034-2.1b4c75ae.woff"
+  },
+  {
+    "revision": "a9b7fac08a3b08df797d3170a531029d",
+    "url": "/Portfolio-React/static/media/TGL 31034-2.a9b7fac0.woff2"
+  },
+  {
+    "revision": "55e235ada147259ca198077d4ba5312a",
+    "url": "/Portfolio-React/static/media/Ticketing.55e235ad.woff"
+  },
+  {
+    "revision": "ea343ad32dc1eb739b7c11bb413e21ba",
+    "url": "/Portfolio-React/static/media/Ticketing.ea343ad3.woff2"
+  },
+  {
+    "revision": "61168692eba12505c2a8a2ddb0b7e7e7",
+    "url": "/Portfolio-React/static/media/Voyager Grotesque(regular).61168692.woff"
+  },
+  {
+    "revision": "f463bda89a30e76534c250aa3ba1eca1",
+    "url": "/Portfolio-React/static/media/Voyager Grotesque(regular).f463bda8.woff2"
+  },
+  {
+    "revision": "013bc58c0c0a3ead247a09ecbd88fe76",
+    "url": "/Portfolio-React/static/media/WarsawGothic.013bc58c.woff"
+  },
+  {
+    "revision": "a9a9a3c2b9e4119a7f39936cebebd460",
+    "url": "/Portfolio-React/static/media/WarsawGothic.a9a9a3c2.woff2"
+  },
+  {
+    "revision": "863a66b8d4f96f9f516a648a831b3574",
+    "url": "/Portfolio-React/static/media/WinterthurCondensedOblique.863a66b8.woff"
+  },
+  {
+    "revision": "ce9098fb29c203a791b5e840d09ee91c",
+    "url": "/Portfolio-React/static/media/WinterthurCondensedOblique.ce9098fb.woff2"
+  },
+  {
+    "revision": "14a20fd7d5ed2fe6ce28ed9b56662cc2",
+    "url": "/Portfolio-React/static/media/YEARR___.14a20fd7.woff2"
+  },
+  {
+    "revision": "2c039d1804c2e69ade5c9cc6f4556a0f",
+    "url": "/Portfolio-React/static/media/YEARR___.2c039d18.woff"
+  },
+  {
+    "revision": "4815c7cc8a1ec314b28d1264929863b1",
+    "url": "/Portfolio-React/static/media/YOLDA___.4815c7cc.woff2"
+  },
+  {
+    "revision": "48df752aef7cf6afe83cc48a6e134417",
+    "url": "/Portfolio-React/static/media/YOLDA___.48df752a.woff"
+  },
+  {
+    "revision": "84a6615b67c3c93cee0e423bcad5e644",
+    "url": "/Portfolio-React/static/media/Ye Old Shire Outline.84a6615b.woff2"
+  },
+  {
+    "revision": "d97bac3f7aa4f2ba683cd8285c05e3b3",
+    "url": "/Portfolio-React/static/media/Ye Old Shire Outline.d97bac3f.woff"
+  },
+  {
+    "revision": "168163acaa897738a93fdcdc064ff8ec",
+    "url": "/Portfolio-React/static/media/Ye Old Shire.168163ac.woff"
+  },
+  {
+    "revision": "7f81a52406658345aeff0cfbfb36ebea",
+    "url": "/Portfolio-React/static/media/Ye Old Shire.7f81a524.woff2"
+  },
+  {
+    "revision": "9b3432178298981f6a761ca06ab91785",
+    "url": "/Portfolio-React/static/media/Young.9b343217.woff"
+  },
+  {
+    "revision": "b18cb89d6256e19fb8c7be43d3ab05a9",
+    "url": "/Portfolio-React/static/media/Young.b18cb89d.woff2"
+  },
+  {
+    "revision": "70193a6cb271996bc343688ea9e097ae",
+    "url": "/Portfolio-React/static/media/bunker.70193a6c.woff"
+  },
+  {
+    "revision": "d7534cfce2d45edece143308a55c669d",
+    "url": "/Portfolio-React/static/media/bunker.d7534cfc.woff2"
+  },
+  {
+    "revision": "988f892e7e70904a5451fde657ba8467",
+    "url": "/Portfolio-React/static/media/cardthumbnail.988f892e.png"
+  },
+  {
+    "revision": "d1543849e7461fe76a13a42aa60a4610",
+    "url": "/Portfolio-React/static/media/cath1-1.d1543849.jpg"
+  },
+  {
+    "revision": "f238c45a39adca38d820498ccbfaec7e",
+    "url": "/Portfolio-React/static/media/cath1.f238c45a.jpg"
+  },
+  {
+    "revision": "633c965e2fac216b09ff10e34302fe02",
+    "url": "/Portfolio-React/static/media/cath2.633c965e.jpg"
+  },
+  {
+    "revision": "274a27ecb30a1d07a1ac4225f25a7641",
+    "url": "/Portfolio-React/static/media/city4.274a27ec.jpg"
+  },
+  {
+    "revision": "d45697ffaad96c379e2a092708c1716f",
+    "url": "/Portfolio-React/static/media/collage3.d45697ff.jpg"
+  },
+  {
+    "revision": "a8a37335e955ae6363f5b376395329cf",
+    "url": "/Portfolio-React/static/media/collage6.a8a37335.jpg"
+  },
+  {
+    "revision": "8f4e25c52818ccd0373b2b0eb19b5c52",
+    "url": "/Portfolio-React/static/media/data8-1.8f4e25c5.jpg"
+  },
+  {
+    "revision": "86c56bb1aaafaa0ed9fe6e92818793f8",
+    "url": "/Portfolio-React/static/media/data8-4.86c56bb1.jpg"
+  },
+  {
+    "revision": "cf2bdfe20251b493404f5a1cd5c307b9",
+    "url": "/Portfolio-React/static/media/data8-7.cf2bdfe2.jpg"
+  },
+  {
+    "revision": "39dd879e7d3040d630de4d219640ecfa",
+    "url": "/Portfolio-React/static/media/data8-8.39dd879e.jpg"
+  },
+  {
+    "revision": "feccc3586acdc73d272b4ec95dabd8de",
+    "url": "/Portfolio-React/static/media/dish.feccc358.png"
+  },
+  {
+    "revision": "fe03b3923bd71fe23ce04000734f3262",
+    "url": "/Portfolio-React/static/media/fun1.fe03b392.png"
+  },
+  {
+    "revision": "96f7e87f395511f90ee21f39e7675fe1",
+    "url": "/Portfolio-React/static/media/gaposiss.96f7e87f.woff2"
+  },
+  {
+    "revision": "aba34cd3bb54d54849e77102023e892f",
+    "url": "/Portfolio-React/static/media/gaposiss.aba34cd3.woff"
+  },
+  {
+    "revision": "a9a46ecbd9219a05c3044fd276795d77",
+    "url": "/Portfolio-React/static/media/giphythumbnail.a9a46ecb.png"
+  },
+  {
+    "revision": "836d1bfdc3f4d93555dbf953e15f7b16",
+    "url": "/Portfolio-React/static/media/glasgow.836d1bfd.png"
+  },
+  {
+    "revision": "625914aefa6f3f4462c1c5d6ac1c47e1",
+    "url": "/Portfolio-React/static/media/googlePF.625914ae.png"
+  },
+  {
+    "revision": "409f8b390510c1bb0e525a7ca23f1045",
+    "url": "/Portfolio-React/static/media/gym1.409f8b39.png"
+  },
+  {
+    "revision": "fb9eee7014fea70c1772242df82092c4",
+    "url": "/Portfolio-React/static/media/gym3.fb9eee70.png"
+  },
+  {
+    "revision": "4e43a577a1d7db94aa8ebfba5c7a96d4",
+    "url": "/Portfolio-React/static/media/hostage1.4e43a577.png"
+  },
+  {
+    "revision": "01c93ae7b127a01c81937c2cbf8951c7",
+    "url": "/Portfolio-React/static/media/house3.01c93ae7.jpg"
+  },
+  {
+    "revision": "ea0b838af761fbaa6d347d5054506470",
+    "url": "/Portfolio-React/static/media/houston4.ea0b838a.png"
+  },
+  {
+    "revision": "007f83b2409a894efb1731d62b2ac368",
+    "url": "/Portfolio-React/static/media/houston5.007f83b2.png"
+  },
+  {
+    "revision": "e07b4bcad0daff9c5fdd679b331cbaf0",
+    "url": "/Portfolio-React/static/media/image3A4454_Glitch10.e07b4bca.jpg"
+  },
+  {
+    "revision": "e4397f66e861c4d39ccfaa4c08ce3f78",
+    "url": "/Portfolio-React/static/media/image3A4454_Glitch3.e4397f66.jpg"
+  },
+  {
+    "revision": "aaa2270db983ba77639a6ab962796326",
+    "url": "/Portfolio-React/static/media/ionic.aaa2270d.woff"
+  },
+  {
+    "revision": "c4fe28f2ece6c75b94afd7adca22a4f0",
+    "url": "/Portfolio-React/static/media/ionic.c4fe28f2.woff2"
+  },
+  {
+    "revision": "2422f467fe8253099f427d08845d305c",
+    "url": "/Portfolio-React/static/media/lirithumbnail.2422f467.png"
+  },
+  {
+    "revision": "657308f1c3208623d43c23e88f6e8a5d",
+    "url": "/Portfolio-React/static/media/london1.657308f1.jpg"
+  },
+  {
+    "revision": "7b771c057d078b5919e747f22321feb6",
+    "url": "/Portfolio-React/static/media/nbadash.7b771c05.png"
+  },
+  {
+    "revision": "3c17a8b668904b7490cbca74b31575a7",
+    "url": "/Portfolio-React/static/media/panda ROBOT.3c17a8b6.woff"
+  },
+  {
+    "revision": "7d8e51a124279c4fa436e15825605e4f",
+    "url": "/Portfolio-React/static/media/panda ROBOT.7d8e51a1.woff2"
+  },
+  {
+    "revision": "36537809e97f7e7246783938181ebe4e",
+    "url": "/Portfolio-React/static/media/repothumbnail.36537809.png"
+  },
+  {
+    "revision": "2da42ec5cb8637894b43d89f099a8c3e",
+    "url": "/Portfolio-React/static/media/res_55E16E01_data7.2da42ec5.jpg"
+  },
+  {
+    "revision": "a59605e59a295173a84b617e99ed7d43",
+    "url": "/Portfolio-React/static/media/res_55E1E564_data7.a59605e5.jpg"
+  },
+  {
+    "revision": "390d8e9e530354c9ac75cfc5d39d4b6b",
+    "url": "/Portfolio-React/static/media/rockdafonkybit.390d8e9e.woff"
+  },
+  {
+    "revision": "4619552073e74adbff379114a5dfa1a8",
+    "url": "/Portfolio-React/static/media/rockdafonkybit.46195520.woff2"
+  },
+  {
+    "revision": "12627b12b5279ac2ea509ef64de0914f",
+    "url": "/Portfolio-React/static/media/sbb-epyx-shadow.12627b12.woff"
+  },
+  {
+    "revision": "e0bdcf57a413b9d7ec1fff29497e3a72",
+    "url": "/Portfolio-React/static/media/sbb-epyx-shadow.e0bdcf57.woff2"
+  },
+  {
+    "revision": "8158dbbc70e9c9d331cd04ae13b9e4d6",
+    "url": "/Portfolio-React/static/media/sendcodes2.8158dbbc.png"
+  },
+  {
+    "revision": "591747b3547104bf66cc761f4b00dccb",
+    "url": "/Portfolio-React/static/media/spacewalk6.591747b3.png"
+  },
+  {
+    "revision": "95cd5432f1f4cd7810246605c7f4e327",
+    "url": "/Portfolio-React/static/media/swim1.95cd5432.png"
+  },
+  {
+    "revision": "32f5ca80776684eccaa9d417c7595aa1",
+    "url": "/Portfolio-React/static/media/tallbw13.32f5ca80.jpg"
+  },
+  {
+    "revision": "a88628b533b4236676c7784a18b149db",
+    "url": "/Portfolio-React/static/media/tallbw8.a88628b5.jpg"
+  },
+  {
+    "revision": "f5a11f712c44d830a01391e105cd3e3b",
+    "url": "/Portfolio-React/static/media/triangle1.f5a11f71.jpg"
+  },
+  {
+    "revision": "eb08d6e51e9775012a9ebffbcd06e366",
+    "url": "/Portfolio-React/static/media/triangle2.eb08d6e5.jpg"
+  },
+  {
+    "revision": "c9b80e8bdf304fec1657a3433a857043",
+    "url": "/Portfolio-React/static/media/triviathumbnail.c9b80e8b.png"
+  },
+  {
+    "revision": "117f22983c6955071b57721d6824d41b",
+    "url": "/Portfolio-React/static/media/wordthumbnail.117f2298.png"
+  },
+  {
+    "revision": "717f6876cc5e6a83d665429b56788726",
+    "url": "/Portfolio-React/static/media/zorque.717f6876.woff"
+  },
+  {
+    "revision": "aea19581dc767aa4140c4c778150deae",
+    "url": "/Portfolio-React/static/media/zorque.aea19581.woff2"
+  }
+]);
